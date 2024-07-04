a9d431fd3903decb51f9d9baa8c1f2e06b1b2e46
Julieta Rabozzi <julieta.rabozzi@artekium.com>
2024-07-04 00:11:13 -0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Artekium on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
